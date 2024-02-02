4a2dcb4e7b9eb72056bc66835663d24991fd9f5c
alberto <pacmokeykong@gmail.com>
2024-02-02 19:45:21 +0100

    MEJORA 2: En esta rama se ha incluido el filtrado por provincia de los registros añadiendo un campo en el que introducimos la provincia y un botón que muestra el resultado de la consulta por provincias

--- app/src/main/java/com/alberto/contactosbasesdedatos/MainActivity.kt
+++ app/src/main/java/com/alberto/contactosbasesdedatos/MainActivity.kt
@@ -12,9 +12,12 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var nameEditText: EditText
     private lateinit var emailEditText: EditText
+    private lateinit var provinceText: EditText
+    private lateinit var intProvince: EditText
     private lateinit var saveButton: Button
     private lateinit var consultaButton: Button
     private lateinit var consultaTextView: TextView
+    private lateinit var consulProv: Button
     private lateinit var db: Databasehandler
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,18 +26,23 @@ class MainActivity : AppCompatActivity() {
 
         nameEditText = findViewById(R.id.nameEditText)
         emailEditText = findViewById(R.id.emailEditText)
+        provinceText = findViewById(R.id.provinceText)
+        intProvince = findViewById(R.id.intProvince)
         saveButton = findViewById(R.id.saveButton)
         consultaButton = findViewById(R.id.consultaButton)
         consultaTextView = findViewById(R.id.consultaTextView)
+        consulProv = findViewById(R.id.consulProv)
+
 
         db = Databasehandler(this)
 
         saveButton.setOnClickListener {
             val name = nameEditText.text.toString().trim()
             val email = emailEditText.text.toString().trim()
+            val province = provinceText.text.toString().trim()
 
-            if (name.isNotEmpty() && email.isNotEmpty()) {
-                val id = db.addContact(name, email)
+            if (name.isNotEmpty() && email.isNotEmpty() && province.isNotEmpty()) {
+                val id = db.addContact(name, email, province)
                 if (id != -1L) {
                     //error al guardar en la base de datos
                     Toast.makeText(
@@ -44,6 +52,7 @@ class MainActivity : AppCompatActivity() {
                     ).show()
                     nameEditText.text.clear()
                     emailEditText.text.clear()
+                    provinceText.text.clear()
 
                 } else {
                     //toast para avisar que se ha guardado el registro en la base de datos
@@ -57,12 +66,24 @@ class MainActivity : AppCompatActivity() {
                 ).show()
             }
         }
+
         consultaButton.setOnClickListener{
 
             val contactList = db.getAllContacts()
+
             for(contact in contactList){
-                Log.d("Contacto ", "ID: ${contact.id}, Nombre: ${contact.name}, Email: ${contact.email}")
-                consultaTextView.append("ID: ${contact.id} ,Nombre: ${contact.name}, Email: ${contact.email} \n ")
+                Log.d("Contacto ", "ID: ${contact.id}, Nombre: ${contact.name}, Email: ${contact.email}, Provincia: ${contact.province}")
+                consultaTextView.append("ID: ${contact.id} ,Nombre: ${contact.name}, Email: ${contact.email}, Provincia: ${contact.province} \n ")
+            }
+        }
+
+        consulProv.setOnClickListener{
+
+            val contactList = db.queryProvinciaContacts(intProvince.text.toString())
+
+            for(contact in contactList){
+                Log.d("Contacto ", "ID: ${contact.id}, Nombre: ${contact.name}, Email: ${contact.email}, Provincia: ${contact.province}")
+                consultaTextView.append("ID: ${contact.id} ,Nombre: ${contact.name}, Email: ${contact.email}, Provincia: ${contact.province} \n ")
             }
         }
     }
